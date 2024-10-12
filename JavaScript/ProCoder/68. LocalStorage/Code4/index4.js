525162673e5aadfc58f44ad593b52743c7fc344b
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-10-12 21:21:00 +0530

    More correction in the index4.js

--- JavaScript/ProCoder/68. LocalStorage/Code4/index4.js
+++ JavaScript/ProCoder/68. LocalStorage/Code4/index4.js
@@ -11,17 +11,19 @@ const myData = JSON.parse(localStorage.getItem('myData'))||{};
 
 
 // Reflecting the value on the load of HTML page on the UI
-const storedData = JSON.parse(localStorage.getItem('myData'));
+// const storedData = JSON.parse(localStorage.getItem('myData'));
 
 // Check if storedData is null
-if (storedData) {
-    nameElement.innerHTML = storedData.name || ''; // Set name if exists
-    ageElement.innerHTML = storedData.age || '';   // Set age if exists
-} else {
-    nameElement.innerHTML = ''; // Set to empty if no data
-    ageElement.innerHTML = '';   // Set to empty if no data
-}
+// if (storedData) {
+//     nameElement.innerHTML = storedData.name || ''; // Set name if exists
+//     ageElement.innerHTML = storedData.age || '';   // Set age if exists
+// } else {
+//     nameElement.innerHTML = ''; // Set to empty if no data
+//     ageElement.innerHTML = '';   // Set to empty if no data
+// }
 
+nameElement.innerHTML = myData.name || '';
+ageElement.innerHTML = myData.age || '';
 
 // Storing the data and overRiding the localStorage values -> name
 nameInput.addEventListener('input', function(event) {
