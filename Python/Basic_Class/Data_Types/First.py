6ec633de6cd781abff1acb461bc38661f626f32a
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-04 14:34:28 +0530

    bool type

--- Python/Basic_Class/Data_Types/First.py
+++ Python/Basic_Class/Data_Types/First.py
@@ -6,3 +6,15 @@ print(type(y))  # Output: <class 'float'>
 
 z = "Hello"
 print(type(z))  # Output: <class 'str'>
+
+# Using the __sizeof__() method 
+
+x= 101223512312321352312312342124123;
+print("Size of x in bytes:", x.__sizeof__());
+#  Size of x in bytes: 40
+
+#  bool type:
+a=True 
+print(a)            # True
+print(type(a))      # <class 'bool'>
+print(int(a))       # 1
