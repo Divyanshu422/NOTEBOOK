f19a8874ed2b6f24ab18cb8fddaf509ce3d0ddf4
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-05 15:01:19 +0530

    Basic Python expression

---
+++ Python/Basic_Class/2. ArithmeticExpression/first.py
@@ -0,0 +1,15 @@
+# Applying the mathematical expression in python
+
+print('Calculating the area of recatangle')
+length = int(input('Enter the length'))
+breadth = int(input('Enter the breadth'))
+print(f'the area of the reactangle is {length * breadth}')
+
+
+# Calculating the Triangle area:
+'''
+print('Calculating the area of Triangle')
+breadth = int(input('Enter the breadth '))
+height = int(input('Enter the height '))
+print(f'the area of the reactangle is {1/2*(breadth * height)}')
+'''
