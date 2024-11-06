8a5f64acea06aa97b9ee9b8156d63e2fbadab5f1
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-06 05:53:19 +0530

    Age Eligibility

---
+++ Python/Basic_Class/3. ConditionalStatement/Third.py
@@ -0,0 +1,27 @@
+# Lecture 4: Student Challenge -> Udemy
+'''
+a = int(input('Enter the First number '))
+b = int(input('Enter the Second number '))
+
+print(f'The difference between two number is {a-b}')
+'''
+
+# Check number is odd or even:
+'''
+num = int(input('Enter a number '))
+
+if num % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
+'''
+
+# Check the age Eligibility to cast a vote
+'''
+age = int(input('Enter the ae of the candidate '))
+
+if age > 18:
+    print('Eligible to cast a vote')
+else: 
+    print('Not eligible to cast a vote')
+'''
---
+++ Python/Basic_Class/3. ConditionalStatement/second.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+c = 15
+
+if a < b and b < c:
+    print("Both conditions are True")
+
+if a == 5 or b == 20:
+    print("At least one condition is True")
+
+if not a > b:
+    print("a is not greater than b")
