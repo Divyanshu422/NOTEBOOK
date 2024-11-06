5c32609990206e0bb2300fdda6f15daa97ab87da
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-06 05:59:01 +0530

    Using the abs() function

--- Python/Basic_Class/3. ConditionalStatement/Third.py
+++ Python/Basic_Class/3. ConditionalStatement/Third.py
@@ -1,10 +1,15 @@
 # Lecture 4: Student Challenge -> Udemy
+# If we want that the difference shall be always positive then either we can use if-else statements [ checking a-b > 0 then print(a-b) or else print (b-a) ]. the other method is using the `abs` function.
+# abs() function, which returns the absolute (positive) value of a number
+
 '''
 a = int(input('Enter the First number '))
 b = int(input('Enter the Second number '))
 
-print(f'The difference between two number is {a-b}')
+print(f'The difference between two number is {abs(a-b)}')
 '''
+
+# ----------------------------------------------------------------
 
 # Check number is odd or even:
 '''
@@ -15,6 +20,8 @@ if num % 2 == 0:
 else:
     print('The number is odd')
 '''
+
+# ----------------------------------------------------------------
 
 # Check the age Eligibility to cast a vote
 '''
