2cbac5bd87f4f51e5c40f1e7f42274722802dbad
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-06 05:31:28 +0530

    if block code;

---
+++ Python/Basic_Class/3. ConditionalStatement/first.py
@@ -0,0 +1,7 @@
+a = int(input('Enter the value of the first variable '))
+b = int(input('Enter the value of the second variable '))
+
+if a > b:
+    print("first variable is greater")
+else:
+    print("second variable is greater")
