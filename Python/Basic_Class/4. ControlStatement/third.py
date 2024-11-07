6a11e166375b02ac49c59b5fa356536682ed046f
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-08 00:00:28 +0530

    Finding the sum in a number

--- Python/Basic_Class/4. ControlStatement/third.py
+++ Python/Basic_Class/4. ControlStatement/third.py
@@ -1,8 +1,25 @@
-# 
+# Counting the number of digit in a number
+
+''' 
 number = int(input('Enter the number '))
 number_of_digit = 0
 
 while number > 0:
-    number = number // 10
+    number = number // 10   # Floor division 
     number_of_digit += 1
 print(f'number of digit are {number_of_digit}')
+'''
+
+# ----------------------------------------------------------------
+
+# Calculating the sum of digits in a number
+'''
+number = int(input('Enter the number '))
+sum = 0
+while number > 0:
+    sum = sum + (number % 10)
+    number = number // 10
+print(f'Sum of digits are {sum}')
+'''
+
+# ----------------------------------------------------------------'''
