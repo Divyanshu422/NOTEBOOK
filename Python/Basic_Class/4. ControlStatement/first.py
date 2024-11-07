6e6e9ef43ee95984972d2bdb07929e6f5505dcd2
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-07 23:37:21 +0530

    Control statement

---
+++ Python/Basic_Class/4. ControlStatement/first.py
@@ -0,0 +1,22 @@
+# Printing the counter till 5
+'''
+counter = 0s
+while counter <= 5:
+    counter += 1
+    print(counter)
+print('Thanks for execution')
+''' 
+
+# Printing the digit in reverse order
+
+number = 62331
+while  number > 0:
+    num = number % 10
+    print(num)
+    number = int(number / 10) # or we can use `//` -> for the floar value
+print('Thanks for execution')
+
+
+
+
+
---
+++ Python/Basic_Class/4. ControlStatement/second.py
@@ -0,0 +1,8 @@
+# Printing the digit in reverse order
+
+number = 62331
+while  number < 0:
+    num = number % 10
+    print(num)
+    nnumber = number / 10
+print('Thanks for execution')
