7b956f7c24abab97d45795996cf45a2dc80d5b3c
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-12 23:26:11 +0530

    Undefined type

--- JavaScript/ZTM/1. JS_Fundamental/script.js
+++ JavaScript/ZTM/1. JS_Fundamental/script.js
@@ -7,7 +7,15 @@
 // console.log(typeof javascriptIsFun); //*  boolean
 
 //* Dynamic typing in javascript
-let javascriptIsFun = false;
-console.log(typeof javascriptIsFun); //*  boolean
-javascriptIsFun = "Yes it is fun to learn";
-console.log(typeof javascriptIsFun); //*  string
+// let javascriptIsFun = false;
+// console.log(typeof javascriptIsFun); //*  boolean
+// javascriptIsFun = "Yes it is fun to learn";
+// console.log(typeof javascriptIsFun); //*  string
+
+//* Undefined typing in javascript
+// let year; // This is valid in JS -> defining the variable and not initializing it
+// console.log(year); //*  undefined
+// console.log(typeof year); //* undefined
+
+// year = 1991;
+// console.log(typeof year); //* number not the int
