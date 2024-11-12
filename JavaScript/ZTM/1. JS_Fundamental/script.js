181809ccf944623919797afd84867031c50b0ec8
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-12 23:16:10 +0530

    Dynamic typing in javaScript

--- JavaScript/ZTM/1. JS_Fundamental/script.js
+++ JavaScript/ZTM/1. JS_Fundamental/script.js
@@ -1,2 +1,13 @@
-let name = "Divyanshu";
-if (name === "Divyanshu") alert("JavaScript is fun to learn");
+// let name = "Divyanshu";
+// if (name === "Divyanshu") alert("JavaScript is fun to learn");
+
+// * Coding 2 : DataType
+
+// let javascriptIsFun = true;
+// console.log(typeof javascriptIsFun); //*  boolean
+
+//* Dynamic typing in javascript
+let javascriptIsFun = false;
+console.log(typeof javascriptIsFun); //*  boolean
+javascriptIsFun = "Yes it is fun to learn";
+console.log(typeof javascriptIsFun); //*  string
