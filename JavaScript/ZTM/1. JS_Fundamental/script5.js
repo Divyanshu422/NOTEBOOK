d28b5cf5f8544a02522a8958ca6c3b383e836122
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-15 14:13:49 +0530

    Adding the tip calculator

---
+++ JavaScript/ZTM/1. JS_Fundamental/script5.js
@@ -0,0 +1,18 @@
+// *    Tip calculator -> Lecture 24
+
+/*
+    let tip, billValue;
+    billValue = prompt("Enter the value of bill");
+
+    tip =
+    billValue >= 50 && billValue <= 300
+        ? (billValue / 100) * 15
+        : (billValue / 100) * 20;
+
+    console.log(
+    `The bill values ${billValue}, the tip was ${tip} and the total value is ${
+        Number(billValue) + Number(tip)
+    } 😇`
+    );
+
+*/
