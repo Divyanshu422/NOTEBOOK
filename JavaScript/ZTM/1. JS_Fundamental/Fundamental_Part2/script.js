7c0f7354362aaaecce536c7cde5b6c31ae37cb12
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-16 19:38:18 +0530

    Function calling the function: FoodProcessor

--- JavaScript/ZTM/1. JS_Fundamental/Fundamental_Part2/script.js
+++ JavaScript/ZTM/1. JS_Fundamental/Fundamental_Part2/script.js
@@ -1,48 +1,83 @@
 // * Understanding strinct mode in js
-/*
-"use strict";
-let hasDriversLicense = false;
-const passTest = false;
 
-if (passTest) {
-  hasDriversLicense = true;
-  console.log("You passed the test!");
-} else {
-  throw new Error("You must pass the test to get a driver's license.");
-}
-*/
+// "use strict";
+// let hasDriversLicense = false;
+// const passTest = false;
+
+// if (passTest) {
+//   hasDriversLicense = true;
+//   console.log("You passed the test!");
+// } else {
+//   throw new Error("You must pass the test to get a driver's license.");
+// }
+
+//* =================**************************======================
 
 // * Function in JavaScript:
 
-/*
-function functionName(parameters) {
-  // Function body
-  return value; // (optional)
-}
+// function functionName(parameters) {
+//   // Function body
+//   return value; // (optional)
+// }
 
-functionName(); // Invoking the function
-*/
+// functionName(); // Invoking the function
+
+//* =================**************************======================
 
 // * Function expression with internal name which is used for recursion
 
 // const greet = function sayHello(param) {
 //   console.log(`Hello ${param}`);
-//     //! the sayHello is used for recursion not for calling/Invoking the function
+//   //! the sayHello is used for recursion not for calling/Invoking the function
+// };
+// sayHello("John");
+
+//* =================**************************======================
+//* Function expression -> without internal name
+
+// const greet = function (param) {
+//   console.log(`Hello My Friend ${param}`);
+// };
+// sayHello("John");
+
+//* =================**************************======================
+// ! Error -> since we are invoking the function using the internal name
+
+// const greet = function sayHello(param) {
+//   console.log(`Hello My Friend ${param} -> welcome back!! `);
+// };
+// sayHello("John"); //! Error
+
+// //* sayHello is the internal name of the function hence
+// //* cant be used to call the function -> can be used for recursive calls
+
+//* =================**************************======================
+
+// ! In JavaScript, if a function is called with fewer arguments than the number of parameters defined, the parameters that do not receive a value are automatically assigned the value undefined.
+// const greet = function (param1, param2, param3) {
+//   console.log(`Hello ${param1}, ${param2}, ${param3}`); //* Hello John, Doe, undefined
 // };
 
-//* Function expression -> without internal name
-/*
-const greet = function (param) {
-  console.log(`Hello My Friend ${param}`);
-};
-sayHello("John"); 
-*/
+// greet("John", "Doe"); // invoking the function with arguments
+//* =================**************************======================
 
-// ! Error
-const greet = function sayHello(param) {
-  console.log(`Hello My Friend ${param} -> welcome back!! `);
-};
-sayHello("John"); //! Error
+//!     What is typeof the function
+// console.log(typeof function () {}); //* function
 
-//* sayHello is the internal name of the function hence
-//* cant be used to call the function -> can be used for recursive calls
+//* =================**************************======================
+
+//! Function calling the function:
+
+const cutFruitPieces = function (fruit) {
+  return 4 * fruit;
+};
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apple and ${orangePieces} oranges is ready`;
+  return juice;
+}
+
+console.log(fruitProcessor(5, 3)); //* Juice with 20 apple and 12 oranges is ready
