ed7b95973eb56c5fbfb3f6701d88f14eb07add2e
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-16 21:17:14 +0530

    Array_Creation

---
+++ JavaScript/ZTM/1. JS_Fundamental/Fundamental_Part2/Arrays/script.js
@@ -0,0 +1,16 @@
+//! Array Declaration:
+
+const friend = ["Divyanshu", "Piyush", "Navjot"];
+console.log(friend); // [ 'Divyanshu', 'Piyush', 'Navjot' ]
+
+const friendArr2 = new Array("Manisha", "Ayushi", "Jeet");
+console.log(friendArr2[2]); // Jeet
+
+const mixedArray = Array.of(1, "hello", true);
+console.log(mixedArray); // Output: [1, "hello", true]
+
+const str = "hello! Divyanshu Good Morning";
+const arrFromString = Array.from(str);
+console.log(arrFromString);
+
+// * =============***************==================
