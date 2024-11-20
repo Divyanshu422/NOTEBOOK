c40f3ea2314bfa544a54067d4c7f0480966ccc62
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-21 04:53:11 +0530

    Selecting all the button on html

--- JavaScript/ZTM/2. DOM_Manipulation/Modal-Window-Project/script.js
+++ JavaScript/ZTM/2. DOM_Manipulation/Modal-Window-Project/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// * Step 1: Selecting all the elements at the top of the file and storing in the variables
+
+const modal = document.querySelector('.modal'); // . -> dot is used for the class
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const openBtns = document.querySelectorAll('.show-modal');
+
+console.log(openBtns);
+
+// Step 2: Looping
