501815dc9a11897b52d8849373e42609a60b77ec
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-21 07:08:42 +0530

    selecting the element and adding the class using classList

--- JavaScript/ZTM/2. DOM_Manipulation/PIG-GAME/script.js
+++ JavaScript/ZTM/2. DOM_Manipulation/PIG-GAME/script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+// Selecting the element
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+
+// Selecting the dice:
+const diceEl = document.querySelector(".dice");
+
+score0El.textContent = 0; // Specifying the number not the string -> Js will automatically convert to a String to display on the screen
+score1El.textContent = 0;
+diceEl.classList.add("hidden"); // Note using the dot notation
