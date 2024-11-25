550d9e71e5f2daf171e152898c9e28f703a90db2
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-25 15:36:22 +0530

    forEach method

--- JavaScript/ZTM/10. Extra/ECMA_Script/1. ForEach.js
+++ JavaScript/ZTM/10. Extra/ECMA_Script/1. ForEach.js
@@ -1,12 +1,40 @@
 //* For each used for iterating over the array:
 
+// let arrData = [6, 10, 20, 14];
+// let sum = 0;
+
+// const sumArray = function (item) {
+//   sum += item;
+// };
+
+// arrData.forEach(sumArray); // Passing the reference of sumArray
+
+// console.log("the sum result is ", sum); // * the sum result is  50
+
+// --------------------------------------------------------------------
+
+//* ForEach using callBack and Anonymous function -> doesnot return anything
+
 let arrData = [6, 10, 20, 14];
-let sum = 0;
 
-const sumArray = function (item) {
-  sum += item;
-};
+// const doubleData = arrData.forEach((item) => {
+//   return 2 * item;
+// });
+// console.log("the double data is ", doubleData); //the double data is  undefined
 
-arrData.forEach(sumArray); // Passing the reference of sumArray
+//! Anonymous function:
 
-console.log("the sum result is ", sum); // * the sum result is  50
+// let tripleData = [];
+// arrData.forEach(function (item) {
+//   tripleData.push(3 * item);
+// });
+// console.log(tripleData); // [18, 30, 60, 42];
+
+// ---------------------
+
+let arrData2 = [6, 10, 20, 14];
+
+arrData2.forEach((item, index) => {
+  arrData2[index] = 2 * item;
+});
+console.log("the double data is ", arrData2); //  [ 12, 20, 40, 28 ]
