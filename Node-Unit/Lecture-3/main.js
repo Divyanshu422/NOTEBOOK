40952df16ab0d33f0544de1da52e79aec18b16f2
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-11-27 12:36:22 +0530

    Understand the global object in Node.js

--- Node-Unit/Lecture-3/main.js
+++ Node-Unit/Lecture-3/main.js
@@ -1,6 +1,17 @@
-var courseName = "Namasta NodeJS";
+// * Running the Js code inside the node environment using command: node ${fileName}.js
+// var courseName = "Namasta NodeJS";
 
-var price = 20;
-var quantity = 1;
+// var price = 20;
+// var quantity = 1;
 
-console.log(`The price to buy ${courseName} course is $${price * quantity} `);
+// console.log(`The price to buy ${courseName} course is $${price * quantity} `);
+
+// ! --------------------------**************************---------------------------
+
+// * Accessing the global object of node environment
+console.log(global);
+
+//* Other way to access the global object in Node.js
+console.log(global === globalThis); // true
+
+// ! --------------------------**************************---------------------------
