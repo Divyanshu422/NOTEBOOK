7e93e305f32636d1118a50c7c2d669951e3c0c7e
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-12-01 15:11:13 +0530

    fetchin the data from fetch_API

--- React/ZTM-NOTES/Lecture 1/monster-rolodex/src/App.js
+++ React/ZTM-NOTES/Lecture 1/monster-rolodex/src/App.js
@@ -1,15 +1,20 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  const monsters = [
-    { id: 1, name: "Frankestein" },
-    { id: 2, name: "Dracula" },
-    { id: 3, name: "zombie" },
-  ];
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((response) => setMonsters(response));
+  }, []);
   return (
     <div className="App">
       {monsters.map((monster) => {
+        console.log(monster.id);
         return (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
+          <div key={monster?.id}>
+            <h1>{monster?.name}</h1>
           </div>
         );
       })}
