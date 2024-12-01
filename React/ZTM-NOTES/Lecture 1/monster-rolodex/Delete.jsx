de892c7be64b949264452500a54fdd318548fd7c
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-12-01 12:13:03 +0530

    Defining the monster Object

---
+++ React/ZTM-NOTES/Lecture 1/monster-rolodex/Delete.jsx
@@ -0,0 +1,19 @@
+// function App() {
+//   const monster = [
+//     { id: 1, name: "Frankestein" },
+//     { id: 2, name: "Dracula" },
+//     { id: 3, name: "zombie" },
+//   ];
+//   return (
+//     <div className="App">
+//       {monster.map((item, id) => {
+//         return (
+//           <div key={id}>
+//             <h1>{item}</h1>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+// export default App;
--- React/ZTM-NOTES/Lecture 1/monster-rolodex/src/App.js
+++ React/ZTM-NOTES/Lecture 1/monster-rolodex/src/App.js
@@ -1,7 +1,18 @@
 function App() {
+  const monsters = [
+    { id: 1, name: "Frankestein" },
+    { id: 2, name: "Dracula" },
+    { id: 3, name: "zombie" },
+  ];
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      {monsters.map((monster) => {
+        return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
