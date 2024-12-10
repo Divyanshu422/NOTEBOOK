927d939a01b5c11a8f248c3a5399523156273540
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-12-10 22:47:57 +0530

    List in python

---
+++ Python/10. List/Program1.py
@@ -0,0 +1,8 @@
+# Code 1: Defining the list and accessing the element of list
+
+my_list = [1, 2, 3, 'apple', True]
+print(my_list[1])   # 2
+print(my_list[-2])  # apple
+
+# Code 2: ways to create the list in python 
+
